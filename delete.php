bb58d185fff1df7a148f6d79ce1aa24e6e88ecec
negihime <t.ishikawa.sys19@morijyobi.ac.jp>
2020-06-29 12:03:12 +0900

    dhz

---
+++ delete.php
@@ -0,0 +1,23 @@
+insert.php
+<?php
+$dsn = 'mysql:dbname=test_DB;host=localhost;';
+$user = 'ishikawa';
+$password = 'morijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_GET['id'];
+
+    $sql = "delete from user where id=:id";
+    $stmt = $dbh->prepare($sql);
+    $prams = array(':id'=> $id);
+    $stmt->execute($prams);
+
+    header('Location: index.php?fg=1');
+    
+} catch (PDOException $e) {
+	header('Location: index.php?fg=2?err='.$e->getMessage());
+	exit();
+}
+?>
